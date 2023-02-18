96669f5c790f4c906f26178bcfc6563f8c491b83
Mohammad Amin Mollazadeh <madamin@pm.me>
2023-02-18 19:47:40 +0330

    fix(Services/Win32ConfigFile): don't start file watcher before service initialization

--- MicaForEveryone/Services/Win32ConfigFile.cs
+++ MicaForEveryone/Services/Win32ConfigFile.cs
@@ -57,6 +57,8 @@ namespace MicaForEveryone.Services
         private bool _isFileWatcherEnabled;
         private string _fileName = "MicaForEveryone.conf";
 
+        private bool _isInitialized = false;
+
         public Win32ConfigFile(IConfigParser parser)
         {
             Parser = parser;
@@ -73,6 +75,7 @@ namespace MicaForEveryone.Services
             set
             {
                 _isFileWatcherEnabled = value;
+                if (_isInitialized == false) return;
                 _fileSystemWatcher.EnableRaisingEvents = value;
             }
         }
@@ -97,6 +100,7 @@ namespace MicaForEveryone.Services
                     }
                 }
                 _fileSystemWatcher.Path = directoryPath;
+                _isInitialized = true;
                 _fileSystemWatcher.EnableRaisingEvents = IsFileWatcherEnabled;
             });
         }
